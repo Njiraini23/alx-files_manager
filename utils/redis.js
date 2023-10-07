5d78f047bff9827599e392aadd5f96e239ed1a9f
Njiraini23 <njiraini88@gmail.com>
2023-10-07 07:36:10 +0000

    redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -3,33 +3,33 @@ import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
+    this.client = createClient();
     this.client.on('error', (err) => {
       console.log(`Redis client not connected to server: ${err}`);
     });
   }
 
-isAlive() {
-  return this.client.connected;
-}
+  isAlive() {
+    return this.client.connected;
+  }
 
-async get(key) {
-  const redisGet = promisify(this.client.get).bind(this.client);
-  const value = await redisGet(key);
-  return value;
-}
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
 
-// the key value pair to redis server
-async set(key, value, time) {
-  const redisSet = promisify(this.client.set).bind(this.client);
-  await redisSet(key, value);
-  await this.client.expire(key, time);
-}
+  // the key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
 
-// del key vale pair from redis server
-async del(key) {
-  const redisDel = promisify(this.client.del).bind(this.client);
-  await redisDel(key);
+  // del key vale pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
   }
 }
 
