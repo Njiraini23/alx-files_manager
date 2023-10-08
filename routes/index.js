3842d21d09cacc9ac6c0941e1220f93b01f65de3
Njiraini23 <njiraini88@gmail.com>
2023-10-08 18:20:59 +0000

    index.js

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
+const router = express.Router();
+
+router.get('/status', AppC
