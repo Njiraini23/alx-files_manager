bd06318d27c8389e31d5af3738998a2655305762
Njiraini23 <njiraini88@gmail.com>
2023-10-09 06:28:48 +0000

    AppController.js

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static async getStatus(req, res) {
+    const radisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
+  }
+
+  static async getStats(req, res) {
+    try {
+      const numUsers = await dbClient.nbUsers();
+      const numFiles = await dbClient.nbFiles();
+
+      res.status(200).json({ users: numUsers, files: numFiles });
+    } catch (error) {
+      console.error('Error retrieving stats:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = AppController;
