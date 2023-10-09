afe9b57b0f791552cda8cca9a07bcb790c70f50c
Njiraini23 <njiraini88@gmail.com>
2023-10-09 15:54:40 +0000

    FilesController.js

---
+++ controllers/FilesController.js
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from 'uuid';
+import { promises as fs } from 'fs';
+import { ObjectID } from 'mongodb';
+import Queue from 'bull';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const fileQueue = new Queue('fileQueue', 'redis://127.0.0.1:6379');
+
+class FilesController {
+  static async getUser(request) {
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbCliet.db.collection('users');
+      const idObject = new ObjectID(userId);
+      const user = await users.findOne({ _id: idObject });
+      if (!user) {
+        return null;
+      }
+      return user;
+    }
+    return null;
+  }
+
+  static async postUpload(request, response) {
+    const user = await FilesController.getUser(request);
+    if (!user) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+    const { name } = request.body;
+    const { type } = request.body;
+    const { parentId } = request.body;
+    const isPublic = request.body.isPublic || false;
+    const { data } = request.body;
+    if (!name) {
+      return response.status(400).json({ error: 'Missing name' });
+    }
+    if (!type) {
+      return response.status(400).json({ error: 'Missing type' });
+    }
+    if (type !== 'folder' && !data) {
+      return response.status(400).json({ error: 'Missing data' });
+    }
+
+    const files = dbClient.db.collection('files');
+    if (parentId) 
