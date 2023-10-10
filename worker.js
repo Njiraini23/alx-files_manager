b4474a72c08cbc64c7e7fb10c1d713c90e867f84
Njiraini23 <njiraini88@gmail.com>
2023-10-10 18:32:15 +0000

    worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,8 @@ import DBClient from './utils/db';
 const Bull = require('bull');
 const { ObjectId } = require('mongodb');
 const imageThumbnail = require('image-thumbnail');
-const fs = require ('fs');
+const fs = require('fs');
+
 const fileQueue = new Bull('fileQueue');
 const userQueue = new Bull('userQueue');
 
@@ -18,27 +19,27 @@ const createImageThumbnail = async (path, options) => {
   }
 };
 
-fileQueue.process(async, (job) => {
-  const { field } = job.data;
-  if (!field) throw Error ('Missing fieldId');
+fileQueue.process(async (job) => {
+  const { fileId } = job.data;
+  if (!fileId) throw Error('Missing fileId');
 
   const { userId } = job.data;
   if (!userId) throw Error('Missing userId');
 
   const fileDocument = await DBClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
-  if (!fileDocument) throw Error ('File not found');
+  if (!fileDocument) throw Error('File not found');
 
-createImageThumbnail(fileDocument.localPath, { width: 500 });
-createImageThumbnail(fileDocument.localPath, { width: 250 });
-createImageThumbnail(fileDocument.localPath, { width: 100 });
+  createImageThumbnail(fileDocument.localPath, { width: 500 });
+  createImageThumbnail(fileDocument.localPath, { width: 250 });
+  createImageThumbnail(fileDocument.localPath, { width: 100 });
 });
 
 userQueue.process(async (job) => {
   const { userId } = job.data;
-  if (!userId) throw Error ('Missing userId');
+  if (!userId) throw Error('Missing userId');
 
   const userDocument = await DBClient.db.collection('users').findOne({ _id: ObjectId(userId) });
-  if (!userDocument) throw Error ('User not found');
+  if (!userDocument) throw Error('User not found');
 
-console.log(`Welcome ${userDocument.email}`);
+  console.log(`Welcome ${userDocument.email}`);
 });
